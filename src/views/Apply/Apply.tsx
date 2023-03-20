56473e2d6053195b443ca82bc52ba74ff5274585
Sheldon Liao <sheldon.liao1015@gmail.com>
2023-03-21 04:20:19 +0800

    feat: 添加审批功能

--- src/views/Apply/Apply.tsx
+++ src/views/Apply/Apply.tsx
@@ -9,13 +9,32 @@ import {
   Space,
   RadioChangeEvent,
   Table,
+  Modal,
+  Form,
+  Select,
+  DatePicker,
+  message,
 } from 'antd';
 import { SearchOutlined } from '@ant-design/icons';
 import type { ColumnsType } from 'antd/es/table';
 import type { Infos } from '../../store/modules/checks';
 import { useAppDispatch, useAppSelector } from '../../store';
-import { getApplyAction, updateApplyList } from '../../store/modules/checks';
+import {
+  getApplyAction,
+  updateApplyList,
+  postApplyAction,
+} from '../../store/modules/checks';
 import _ from 'lodash';
+import 'dayjs/locale/zh-cn';
+import locale from 'antd/es/date-picker/locale/zh_CN';
+import dayjs from 'dayjs';
+
+interface FormInfos {
+  approvername: string;
+  note: string;
+  reason: string;
+  time: [string, string];
+}
 
 const approverTypes = [
   { label: '全部', value: '全部' },
@@ -43,6 +62,7 @@ const columns: ColumnsType<Infos> = [
     title: '时间',
     dataIndex: 'time',
     key: 'time',
+    render: (_) => _.join(' - '),
   },
   {
     title: '备注',
@@ -73,6 +93,8 @@ export default function Apply() {
       (v.note as string).includes(searchWord)
   );
   const dispatch = useAppDispatch();
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [form] = Form.useForm();
 
   const approverTypeChange = (ev: RadioChangeEvent) => {
     setApproverType(ev.target.value);
@@ -80,6 +102,57 @@ export default function Apply() {
   const searchWordChange = (ev: React.ChangeEvent<HTMLInputElement>) => {
     setSearchWord(ev.target.value);
   };
+
+  const showModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const handleOk = () => {
+    setIsModalOpen(false);
+  };
+
+  const handleCancel = () => {
+    setIsModalOpen(false);
+    handleReset();
+  };
+
+  const handleReset = () => {
+    form.resetFields();
+  };
+
+  const onFinish = (values: FormInfos) => {
+    values.time[0] = dayjs(values.time[0]).format('YYYY-MM-DD hh:mm:ss');
+    values.time[1] = dayjs(values.time[1]).format('YYYY-MM-DD hh:mm:ss');
+    const applyList = {
+      ...values,
+      applicantid: usersInfos._id as string,
+      applicantname: usersInfos.name as string,
+      approverid:
+        Array.isArray(usersInfos.approver) &&
+        usersInfos.approver.find((item) => item.name === values.approvername)
+          ._id,
+    };
+    dispatch(postApplyAction(applyList)).then((action) => {
+      const { errcode } = (action.payload as { [index: string]: unknown })
+        .data as { [index: string]: unknown };
+      if (errcode === 0) {
+        message.success('添加审批成功');
+        handleCancel();
+        dispatch(
+          getApplyAction({ applicantid: usersInfos._id as string })
+        ).then((action) => {
+          const { errcode, rets } = (
+            action.payload as { [index: string]: unknown }
+          ).data as { [index: string]: unknown };
+          if (errcode === 0) {
+            dispatch(updateApplyList(rets as Infos[]));
+          }
+        });
+      }
+    });
+  };
+
+  const onFinishFailed = () => {};
 
   useEffect(() => {
     if (_.isEmpty(applyList)) {
@@ -99,7 +172,9 @@ export default function Apply() {
   return (
     <div>
       <Row className={styles['apply-title']} justify="space-between">
-        <Button type="primary">添加审批</Button>
+        <Button type="primary" onClick={showModal}>
+          添加审批
+        </Button>
         <Space>
           <Input
             placeholder="请输入搜索关键词"
@@ -125,11 +200,80 @@ export default function Apply() {
         columns={columns}
         className={styles['apply-table']}
         bordered
-        size="small"
+        // size="small"
         pagination={{
           defaultPageSize: 5,
         }}
       />
+      <Modal
+        title="添加审批"
+        open={isModalOpen}
+        onOk={handleOk}
+        onCancel={handleCancel}
+        footer={null}
+      >
+        <Form
+          name="basic"
+          labelCol={{ span: 4 }}
+          wrapperCol={{ span: 20 }}
+          onFinish={onFinish}
+          onFinishFailed={onFinishFailed}
+          className={styles['apply-form']}
+          autoComplete="off"
+          form={form}
+        >
+          <Form.Item
+            label="审批人"
+            name="approvername"
+            rules={[{ required: true, message: '请选择审批人!' }]}
+          >
+            <Select placeholder="请选择审批人" allowClear>
+              {Array.isArray(usersInfos.approver) &&
+                usersInfos.approver.map((item) => (
+                  <Select.Option key={item._id} value={item.name}>
+                    {item.name}
+                  </Select.Option>
+                ))}
+            </Select>
+          </Form.Item>
+
+          <Form.Item
+            label="审批事由"
+            name="reason"
+            rules={[{ required: true, message: '请选择审批事由' }]}
+          >
+            <Select placeholder="请选择审批事由" allowClear>
+              <Select.Option value="年假">年假</Select.Option>
+              <Select.Option value="事假">事假</Select.Option>
+              <Select.Option value="病假">病假</Select.Option>
+              <Select.Option value="外出">外出</Select.Option>
+              <Select.Option value="补签卡">补签卡</Select.Option>
+            </Select>
+          </Form.Item>
+          <Form.Item
+            label="时间"
+            name="time"
+            rules={[{ required: true, message: '请选择审批时间' }]}
+          >
+            <DatePicker.RangePicker showTime locale={locale} />
+          </Form.Item>
+          <Form.Item
+            label="备注"
+            name="note"
+            rules={[{ required: true, message: '请输入备注' }]}
+          >
+            <Input.TextArea placeholder="请输入备注" />
+          </Form.Item>
+          <Row justify="end">
+            <Space>
+              <Button onClick={handleReset}>重置</Button>
+              <Button type="primary" htmlType="submit">
+                提交
+              </Button>
+            </Space>
+          </Row>
+        </Form>
+      </Modal>
     </div>
   );
 }
