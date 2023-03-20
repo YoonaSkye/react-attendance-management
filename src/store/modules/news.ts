2ee62261eeabe8ad8cd94f3155b3c3d88de38946
Sheldon Liao <sheldon.liao1015@gmail.com>
2023-03-21 05:10:48 +0800

    feat: 审批页功能实现与更新审批信息

--- src/store/modules/news.ts
+++ src/store/modules/news.ts
@@ -1,12 +1,39 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import type { PayloadAction } from '@reduxjs/toolkit';
+import http from '../../http';
 
-type Info = {
+export type Info = {
   [index: string]: unknown;
 };
+
 type NewsState = {
   info: Info;
 };
+
+type GetRemind = {
+  userid: string;
+};
+type PutRemind = {
+  userid: string;
+  applicant?: boolean;
+  approver?: boolean;
+};
+
+export const getRemindAction = createAsyncThunk(
+  'news/getRemindAction',
+  async (payload: GetRemind) => {
+    const ret = await http.get('/news/remind', payload);
+    return ret;
+  }
+);
+
+export const putRemindAction = createAsyncThunk(
+  'news/putRemindAction',
+  async (payload: PutRemind) => {
+    const ret = await http.put('/news/remind', payload);
+    return ret;
+  }
+);
 
 const newsSlice = createSlice({
   name: 'news',
